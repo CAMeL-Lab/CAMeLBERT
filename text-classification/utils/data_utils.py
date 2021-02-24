26dd5f92b7df23e7e42c8801fd4475dc71ffb579
balhafni <alhafni@usc.edu>
2021-02-24 21:46:26 +0400

    Fixing header

--- text-classification/utils/data_utils.py
+++ text-classification/utils/data_utils.py
@@ -24,7 +24,6 @@
 
 """
 Text classification utils
-Heavily adapted from: 
 """
 
 import logging
@@ -148,8 +147,8 @@ def convert_examples_to_features(examples, tokenizer,
             input_ids = ([pad_token] * padding_length) + input_ids
             attention_mask = ([0 if mask_padding_with_zero else 1]
                               * padding_length) + attention_mask
-            token_type_ids = ([pad_token_segment_id] * padding_length)
-                              + token_type_ids
+            token_type_ids = (([pad_token_segment_id] * padding_length)
+                              + token_type_ids)
         else:
             input_ids = input_ids + ([pad_token] * padding_length)
             attention_mask = attention_mask + ([0 if mask_padding_with_zero
@@ -157,15 +156,17 @@ def convert_examples_to_features(examples, tokenizer,
             token_type_ids = token_type_ids + ([pad_token_segment_id]
                                                * padding_length)
 
-        assert len(input_ids) == max_length,
+        assert len(input_ids) == max_length, (
                 "Error with input length {} vs {}".format(len(input_ids),
-                                                          max_length)
-        assert len(attention_mask) == max_length,
+                                                          max_length))
+
+        assert len(attention_mask) == max_length, (
                 "Error with input length {} vs {}".format(len(attention_mask),
-                                                          max_length)
-        assert len(token_type_ids) == max_length,
+                                                          max_length))
+
+        assert len(token_type_ids) == max_length, (
                 "Error with input length {} vs {}".format(len(token_type_ids),
-                                                          max_length)
+                                                          max_length))
 
         if output_mode == "classification":
             label = label_map[example.label]
