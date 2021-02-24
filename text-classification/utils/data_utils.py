8c6b4728080c1d33065c10a8efe4df77c849a033
balhafni <alhafni@usc.edu>
2021-02-24 11:44:35 -0500

    Text classification utils

--- text-classification/utils/data_utils.py
+++ text-classification/utils/data_utils.py
@@ -2,7 +2,7 @@
 
 # MIT License
 #
-# Copyright 2018-2019 New York University Abu Dhabi
+# Copyright 2018-2021 New York University Abu Dhabi
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -23,20 +23,20 @@
 # SOFTWARE.
 
 """
-Most parts of this script are adapted from the code of Wolf et al. https://arxiv.org/abs/1910.03771
-Which is available at: https://github.com/huggingface/transformers
+Text classification utils
+Heavily adapted from: 
 """
 
 import logging
 import os
+import re
 import camel_tools.utils.normalize as normalize
 import camel_tools.utils.dediac as dediac
-import re
-from transformers.data.processors.utils import DataProcessor, InputExample, InputFeatures
-from transformers.file_utils import is_tf_available
-
-if is_tf_available():
-    import tensorflow as tf
+from transformers.data.processors.utils import (
+    DataProcessor,
+    InputExample,
+    InputFeatures
+)
 
 logger = logging.getLogger(__name__)
 
@@ -70,49 +70,56 @@ NADI_COUNTRY_LABELS = ["Algeria", "Bahrain", "Djibouti", "Egypt",
 
 
 def convert_examples_to_features(examples, tokenizer,
-                                      max_length=512,
-                                      task=None,
-                                      label_list=None,
-                                      output_mode=None,
-                                      pad_on_left=False,
-                                      pad_token=0,
-                                      pad_token_segment_id=0,
-                                      mask_padding_with_zero=True):
+                                max_length=512,
+                                task=None,
+                                label_list=None,
+                                output_mode=None,
+                                pad_on_left=False,
+                                pad_token=0,
+                                pad_token_segment_id=0,
+                                mask_padding_with_zero=True):
     """
     Loads a data file into a list of ``InputFeatures``
 
     Args:
-        examples: List of ``InputExamples`` or ``tf.data.Dataset`` containing the examples.
+        examples: List of ``InputExamples`` containing the examples.
         tokenizer: Instance of a tokenizer that will tokenize the examples
         max_length: Maximum example length
         task: Arabic Sentiment Analysis task
-        label_list: List of labels. Can be obtained from the processor using the ``processor.get_labels()`` method
-        output_mode: String indicating the output mode. Either ``regression`` or ``classification``
-        pad_on_left: If set to ``True``, the examples will be padded on the left rather than on the right (default)
+        label_list: List of labels.
+                    Can be obtained from the processor using the
+                    ``processor.get_labels()`` method
+        output_mode: String indicating the output mode.
+                     Either ``regression`` or ``classification``
+        pad_on_left: If set to ``True``,
+                     the examples will be padded on the left rather
+                     than on the right (default)
         pad_token: Padding token
-        pad_token_segment_id: The segment ID for the padding token (It is usually 0, but can vary such as for XLNet where it is 4)
-        mask_padding_with_zero: If set to ``True``, the attention mask will be filled by ``1`` for actual values
-            and by ``0`` for padded values. If set to ``False``, inverts it (``1`` for padded values, ``0`` for
-            actual values)
+        pad_token_segment_id: The segment ID for the padding token
+                              (It is usually 0, but can vary such as for
+                               XLNet where it is 4)
+        mask_padding_with_zero: If set to ``True``,
+                                the attention mask will be filled by ``1``
+                                for actual values
+                                and by ``0`` for padded values.
+                                If set to ``False``, inverts it (``1`` for
+                                padded values, ``0`` for actual values)
 
     Returns:
-        If the ``examples`` input is a ``tf.data.Dataset``, will return a ``tf.data.Dataset``
-        containing the task-specific features. If the input is a list of ``InputExamples``, will return
-        a list of task-specific ``InputFeatures`` which can be fed to the model.
+        list of task-specific ``InputFeatures`` which can be fed to the model.
 
     """
-    is_tf_dataset = False
-    if is_tf_available() and isinstance(examples, tf.data.Dataset):
-        is_tf_dataset = True
 
     if task is not None:
         processor = processors[task]()
         if label_list is None:
             label_list = processor.get_labels()
-            logger.info("Using label list %s for task %s" % (label_list, task))
+            logger.info("Using label list %s for task %s" %
+                        (label_list, task))
         if output_mode is None:
             output_mode = output_modes[task]
-            logger.info("Using output mode %s for task %s" % (output_mode, task))
+            logger.info("Using output mode %s for task %s" %
+                        (output_mode, task))
 
     label_map = {label: i for i, label in enumerate(label_list)}
     logger.info('**LABEL MAP**')
@@ -121,8 +128,6 @@ def convert_examples_to_features(examples, tokenizer,
     for (ex_index, example) in enumerate(examples):
         if ex_index % 10000 == 0:
             logger.info("Writing example %d" % (ex_index))
-        if is_tf_dataset:
-            example = processor.get_example_from_tensor_dict(example)
 
         inputs = tokenizer.encode_plus(
             example.text_a,
@@ -141,18 +146,26 @@ def convert_examples_to_features(examples, tokenizer,
         padding_length = max_length - len(input_ids)
         if pad_on_left:
             input_ids = ([pad_token] * padding_length) + input_ids
-            attention_mask = ([0 if mask_padding_with_zero else 1] * padding_length) + attention_mask
-            token_type_ids = ([pad_token_segment_id] * padding_length) + token_type_ids
+            attention_mask = ([0 if mask_padding_with_zero else 1]
+                              * padding_length) + attention_mask
+            token_type_ids = ([pad_token_segment_id] * padding_length)
+                              + token_type_ids
         else:
             input_ids = input_ids + ([pad_token] * padding_length)
-            attention_mask = attention_mask + ([0 if mask_padding_with_zero else 1] * padding_length)
-            token_type_ids = token_type_ids + ([pad_token_segment_id] * padding_length)
+            attention_mask = attention_mask + ([0 if mask_padding_with_zero
+                                                  else 1] * padding_length)
+            token_type_ids = token_type_ids + ([pad_token_segment_id]
+                                               * padding_length)
 
-        assert len(input_ids) == max_length, "Error with input length {} vs {}".format(len(input_ids), max_length)
-        assert len(attention_mask) == max_length, "Error with input length {} vs {}".format(len(attention_mask),
-                                                                                            max_length)
-        assert len(token_type_ids) == max_length, "Error with input length {} vs {}".format(len(token_type_ids),
-                                                                                            max_length)
+        assert len(input_ids) == max_length,
+                "Error with input length {} vs {}".format(len(input_ids),
+                                                          max_length)
+        assert len(attention_mask) == max_length,
+                "Error with input length {} vs {}".format(len(attention_mask),
+                                                          max_length)
+        assert len(token_type_ids) == max_length,
+                "Error with input length {} vs {}".format(len(token_type_ids),
+                                                          max_length)
 
         if output_mode == "classification":
             label = label_map[example.label]
@@ -164,9 +177,12 @@ def convert_examples_to_features(examples, tokenizer,
         if ex_index < 5:
             logger.info("*** Example ***")
             logger.info("guid: %s" % (example.guid))
-            logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
-            logger.info("attention_mask: %s" % " ".join([str(x) for x in attention_mask]))
-            logger.info("token_type_ids: %s" % " ".join([str(x) for x in token_type_ids]))
+            logger.info("input_ids: %s" %
+                        " ".join([str(x) for x in input_ids]))
+            logger.info("attention_mask: %s" %
+                        " ".join([str(x) for x in attention_mask]))
+            logger.info("token_type_ids: %s" %
+                        " ".join([str(x) for x in token_type_ids]))
             logger.info("label: %s (id = %d)" % (example.label, label))
 
         features.append(
@@ -174,24 +190,6 @@ def convert_examples_to_features(examples, tokenizer,
                           attention_mask=attention_mask,
                           token_type_ids=token_type_ids,
                           label=label))
-
-    if is_tf_available() and is_tf_dataset:
-        def gen():
-            for ex in features:
-                yield ({'input_ids': ex.input_ids,
-                        'attention_mask': ex.attention_mask,
-                        'token_type_ids': ex.token_type_ids},
-                       ex.label)
-
-        return tf.data.Dataset.from_generator(gen,
-                                              ({'input_ids': tf.int32,
-                                                'attention_mask': tf.int32,
-                                                'token_type_ids': tf.int32},
-                                               tf.int64),
-                                              ({'input_ids': tf.TensorShape([None]),
-                                                'attention_mask': tf.TensorShape([None]),
-                                                'token_type_ids': tf.TensorShape([None])},
-                                               tf.TensorShape([])))
 
     return features
 
@@ -213,25 +211,29 @@ class ArabicSentimentProcessor(DataProcessor):
             text_a = line[1]
             text_a = self.process_tweet(line[1])
             label = line[2]
-            examples.append(InputExample(guid=guid, text_a=text_a, label=label))
+            examples.append(InputExample(guid=guid, text_a=text_a,
+                                         label=label))
         return examples
 
     def get_train_examples(self, data_dir):
         """See base class."""
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "train.tsv")))
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir,
+                                           "train.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "train.tsv")),
             "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "dev.tsv")))
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir,
+                                           "dev.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "dev.tsv")),
             "dev")
 
     def get_test_examples(self, data_dir):
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "test.tsv")))
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir,
+                                           "test.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "ArSAS.test.tsv")),
             "test")
@@ -285,26 +287,30 @@ class ArabicPoetryProcessor(DataProcessor):
                 text_a = self.process_verse(line[0])
                 text_b = None
                 label = line[1]
-            examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+            examples.append(InputExample(guid=guid, text_a=text_a,
+                                         text_b=text_b, label=label))
 
         return examples
 
     def get_train_examples(self, data_dir):
         """See base class."""
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "train.tsv")))
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir,
+                                           "train.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "train.tsv")),
             "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "dev.tsv")))
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir,
+                                           "dev.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "dev.tsv")),
             "dev")
 
     def get_test_examples(self, data_dir):
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "test.tsv")))
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir,
+                                           "test.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "test.tsv")),
             "test")
@@ -348,25 +354,25 @@ class ArabicDIDProcessor_MADAR_26(DataProcessor):
     def get_train_examples(self, data_dir):
         """See base class."""
         logger.info("LOOKING AT {}".format(os.path.join(data_dir,
-                    "MADAR-Corpus-26-train.tsv")))
+                                           "MADAR-Corpus-26-train.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "MADAR-Corpus-26-train.tsv")),
-            "train")
+                                        "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
         logger.info("LOOKING AT {}".format(os.path.join(data_dir,
-                    "MADAR-Corpus-26-dev.tsv")))
+                                           "MADAR-Corpus-26-dev.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "MADAR-Corpus-26-dev.tsv")),
-            "dev")
+                           "dev")
 
     def get_test_examples(self, data_dir):
         logger.info("LOOKING AT {}".format(os.path.join(data_dir,
-                    "MADAR-Corpus-26-test.tsv")))
+                                           "MADAR-Corpus-26-test.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "MADAR-Corpus-26-test.tsv")),
-            "test")
+                           "test")
 
     def get_labels(self):
         return MADAR_26_LABELS
@@ -411,7 +417,7 @@ class ArabicDIDProcessor_MADAR_6(DataProcessor):
                     "MADAR-Corpus-6-train.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "MADAR-Corpus-6-train.tsv")),
-            "train")
+                                        "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
@@ -419,7 +425,7 @@ class ArabicDIDProcessor_MADAR_6(DataProcessor):
                     "MADAR-Corpus-6-dev.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "MADAR-Corpus-6-dev.tsv")),
-            "dev")
+                                        "dev")
 
     def get_labels(self):
         return MADAR_6_LABELS
@@ -456,28 +462,32 @@ class ArabicDIDProcessor_MADAR_Twitter(DataProcessor):
             user_id = line[0]
             text_a = self.process_tweet(line[1])
             label = line[2]
-            examples.append(InputExample(guid=guid, text_a=text_a, label=label))
+            examples.append(InputExample(guid=guid, text_a=text_a,
+                                         label=label))
         return examples
 
     def get_train_examples(self, data_dir):
         """See base class."""
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "grouped_tweets.train.tsv")))
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir,
+                                           "grouped_tweets.train.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "grouped_tweets.train.tsv")),
-            "train")
+                                        "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "grouped_tweets.dev.tsv")))
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir,
+                                           "grouped_tweets.dev.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "grouped_tweets.dev.tsv")),
-            "dev")
+                                        "dev")
 
     def get_test_examples(self, data_dir):
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "grouped_tweets.test.tsv")))
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir,
+                                           "grouped_tweets.test.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "grouped_tweets.test.tsv")),
-            "test")
+                                        "test")
 
     def get_labels(self):
         return MADAR_TWITTER_LABELS
@@ -521,28 +531,32 @@ class ArabicDIDProcessor_NADI_COUNTRY(DataProcessor):
             guid = "%s-%s" % (set_type, i)
             text_a = self.process_tweet(line[1])
             label = line[2]
-            examples.append(InputExample(guid=guid, text_a=text_a, label=label))
+            examples.append(InputExample(guid=guid, text_a=text_a,
+                                         label=label))
         return examples
 
     def get_train_examples(self, data_dir):
         """See base class."""
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "train_labeled.tsv")))
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir,
+                                           "train_labeled.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "train_labeled.tsv")),
-            "train")
+                                        "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "dev_labeled.tsv")))
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir,
+                                           "dev_labeled.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "dev_labeled.tsv")),
-            "dev")
+                                        "dev")
 
     def get_test_examples(self, data_dir):
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "test.tsv")))
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir,
+                                           "test.tsv")))
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "test.tsv")),
-            "test")
+                                        "test")
 
     def get_labels(self):
         return NADI_COUNTRY_LABELS
