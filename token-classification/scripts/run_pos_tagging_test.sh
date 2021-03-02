8621895d865df23b723000d3694746cf7ced7624
balhafni <alhafni@usc.edu>
2021-03-03 01:00:59 +0400

    POS tagging test eval script

---
+++ token-classification/scripts/run_pos_tagging_test.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#SBATCH -p condo 
+# use gpus
+#SBATCH --gres=gpu:1
+# memory
+#SBATCH --mem=120000
+# Walltime format hh:mm:ss
+#SBATCH --time=11:30:00
+# Output and error files
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
+
+nvidia-smi
+module purge
+
+#################################
+# POS TAGGING TEST EVAL SCRIPT
+#################################
+
+# aubmindlab/bert-base-arabertv01
+# lanwuwei/GigaBERT-v4-Arabic-and-English
+# bashar-talafha/multi-dialect-bert-base-arabic
+# asafaya/bert-base-arabic
+# /scratch/nlp/CAMeLBERT/model/bert-base-wp-30k_msl-512-CA-full-1000000-step
+# /scratch/nlp/CAMeLBERT/model/bert-base-wp-30k_msl-512-MSA-full-1000000-step
+# /scratch/nlp/CAMeLBERT/model/bert-base-wp-30k_msl-512-MIX-full-1000000-step
+# /scratch/nlp/CAMeLBERT/model/bert-base-wp-30k_msl-512-DA-full-1000000-step
+# /scratch/nlp/CAMeLBERT/model/bert-base-wp-30k_msl-512-MSA-half-1000000-step
+# /scratch/nlp/CAMeLBERT/model/bert-base-wp-30k_msl-512-MSA-quarter-1000000-step
+# /scratch/nlp/CAMeLBERT/model/bert-base-wp-30k_msl-512-MSA-eighth-1000000-step
+# /scratch/nlp/CAMeLBERT/model/bert-base-wp-30k_msl-512-MSA-sixteenth-1000000-step
+# bert-base-multilingual-cased
+# /scratch/ba63/UBC-NLP/MARBERT
+# /scratch/ba63/UBC-NLP/ARBERT
+# /scratch/ba63/bert-base-arabertv02/
+
+export DATA_DIR=/scratch/ba63/magold_files/GULF
+export MAX_LENGTH=512
+export OUTPUT_DIR=/scratch/ba63/fine_tuned_models/pos_models/GULF/CAMeLBERT_CA_POS_GULF/checkpoint-3500-best
+export BATCH_SIZE=32
+export NUM_EPOCHS=10
+export SAVE_STEPS=500
+export SEED=12345
+
+
+python run_token_classification.py \
+--data_dir $DATA_DIR \
+--labels $DATA_DIR/labels.txt \
+--model_name_or_path $OUTPUT_DIR \
+--output_dir $OUTPUT_DIR \
+--max_seq_length  $MAX_LENGTH \
+--num_train_epochs $NUM_EPOCHS \
+--per_device_train_batch_size $BATCH_SIZE \
+--save_steps $SAVE_STEPS \
+--seed $SEED \
+--overwrite_cache \
+--do_pred
